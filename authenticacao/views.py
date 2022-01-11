ae23032afd8d87303620baf0679ee130663b0453
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-11 13:49:01 -0400

    request_method_upd

--- authenticacao/views.py
+++ authenticacao/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
-    return render(request, "cadastro.html")
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+    elif request.method == "POST":
+        return HttpResponse("teste")
 
 
 def login(request):
