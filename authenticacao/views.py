eec009a5dbd24a0957957cd4c078be93d31a1b43
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-11 14:13:18 -0400

    first_verifi_upd

--- authenticacao/views.py
+++ authenticacao/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, "cadastro.html")
     elif request.method == "POST":
-        print(request.POST)
-        return HttpResponse("teste")
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("senha")
+        
+        if len(username.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0:
+            return redirect('/auth/cadastro')
+
+        return HttpResponse(f"{username}:{email}:{password}")
+
 
 
 def login(request):
