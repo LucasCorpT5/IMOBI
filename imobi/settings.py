c33a4837208119c86b868bc1ae1558050fdc6bbe
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-11 14:43:42 -0400

    import_constants

--- imobi/settings.py
+++ imobi/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+from django.contrib import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
