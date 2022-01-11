6f769e28ba9a91811060b1e973d6f860e8693ae0
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-11 14:41:54 -0400

    cad_verific_upd2

--- authenticacao/views.py
+++ authenticacao/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
@@ -10,12 +10,22 @@ def cadastro(request):
         username = request.POST.get("username")
         email = request.POST.get("email")
         password = request.POST.get("senha")
-        
+
         if len(username.strip()) == 0 or len(email.strip()) == 0 or len(password.strip()) == 0:
             return redirect('/auth/cadastro')
 
-        return HttpResponse(f"{username}:{email}:{password}")
+        user = User.objects.filter(username=username)
+        
+        if user.exists():
+            return redirect('/auth/cadastro')
 
+        try:
+            user = User(username=username, email=email, password=password)
+
+            user.save()
+            return redirect('/auth/login')
+        except:
+            return redirect('/auth/cadastro')
 
 
 def login(request):
