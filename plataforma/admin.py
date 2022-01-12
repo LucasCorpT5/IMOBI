9f87f1230f27c82994377b2884f9b1708516458d
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-12 19:22:07 -0400

    admin_register_upd1

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Imagem, Cidade, DiasVisita, Horario, Imovei, Visitas
+
+@admin.register(Imovei)
+class ImoveiAdmin(admin.ModelAdmin):
+    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
 
 # Register your models here.
+admin.site.register(DiasVisita)
+admin.site.register(Cidade)
+admin.site.register(Imagem)
+admin.site.register(Horario)
+admin.site.register(Imovei)
+admin.site.register(Visitas)
