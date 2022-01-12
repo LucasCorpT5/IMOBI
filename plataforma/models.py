df49f89da28dbe99fc121192906ee369850ac1cf
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-12 18:24:21 -0400

    img_init_upd1

--- plataforma/models.py
+++ plataforma/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Imagem(models.Model):
+    img = models.ImageField(upload_to='img')
--- plataforma/views.py
+++ plataforma/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url="/auth/login")
 def home(request):
     return HttpResponse("Estou na home")
