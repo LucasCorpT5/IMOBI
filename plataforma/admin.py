e93543421fafb1f7b165c1b522d1d8785a172995
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-12 20:59:33 -0400

    imoveis_all_uppd1

--- plataforma/admin.py
+++ plataforma/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
 from .models import Imagem, Cidade, DiasVisita, Horario, Imovei, Visitas
 
-@admin.register(Imovei)
-class ImoveiAdmin(admin.ModelAdmin):
-    list_display = ('rua', 'valor', 'quartos', 'tamanho', 'cidade', 'tipo')
-    list_editable = ('valor', 'tipo')
-    list_filte = ('cidade', 'tipo')
-
 # Register your models here.
 admin.site.register(DiasVisita)
 admin.site.register(Cidade)
--- plataforma/views.py
+++ plataforma/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from  .models import Imovei
 
 # Create your views here.
 @login_required(login_url="/auth/login")
 def home(request):
-    return HttpResponse("Estou na home")
+    imoveis = Imovei.objects.all()
+    return render(request, 'home.html', {'imoveis': imoveis})
