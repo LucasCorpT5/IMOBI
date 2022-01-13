b04e6f85ef10a4144fd2a2b4319a0482fa38428d
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-01-12 22:02:26 -0400

    preco_maximo_and_min

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,10 +1,24 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from  .models import Imovei
+from  .models import Imovei, Cidade
 
 # Create your views here.
 @login_required(login_url="/auth/login")
 def home(request):
+    preco_minimo = request.GET.get("preco_minimo")
+    preco_maximo = request.GET.get("preco_maximo")
+    cidade = request.GET.get("cidade")
+    tipo = request.GET.getlist("tipo")
+
+    if preco_minimo or preco_maximo or cidade or tipo:
+        
+        if not preco_minimo:
+            preco_minimo = 0
+        if not preco_maximo:
+            preco_maximo = 9999999999999999
+
+    print(f'PM {preco_minimo} PMAX {preco_maximo} cidade {cidade} tipo {tipo}')
     imoveis = Imovei.objects.all()
-    return render(request, 'home.html', {'imoveis': imoveis})
+    cidades = Cidade.objects.all()
+    return render(request, 'home.html', {'imoveis': imoveis, 'cidades': cidades})
